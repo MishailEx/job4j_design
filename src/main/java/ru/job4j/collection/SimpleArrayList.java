282efdfeb04dc7c7ba38c19192707cc0aa808c31
MishailEx <Mishail.avram@mail.ru>
2021-08-26 19:47:13 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,84 @@
+package ru.job4j.collection;
+
+import ru.job4j.list.List;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements List<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length - 1) {
+            container =  Arrays.copyOf(container, container.length * 2);
+        }
+        container[size] = value;
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, container.length);
+        T tmp = container[index];
+        container[index] = newValue;
+        modCount++;
+        return tmp;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T tmp = container[index];
+        System.arraycopy(container, index + 1, container, index,
+                container.length - 1 - index);
+        container[container.length - 1] = null;
+        modCount++;
+        size--;
+        return tmp;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int point = 0;
+            int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                return  point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount < modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[point++];
+            }
+
+        };
+    }
+}
