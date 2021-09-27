13c78d80bb9e38fd0fa99a3c9b7876299b22f00b
MishailEx <Mishail.avram@mail.ru>
2021-09-27 11:05:56 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Analizy {
+    private String marker = "200";
+    private String key;
+    public void unavailable(String source, String target) {
+        Map<String, String> log = new LinkedHashMap<>();
+        try (BufferedReader out = new BufferedReader(new FileReader(source))) {
+            out.lines()
+                    .map(e -> e.split("\\s"))
+                    .forEach(a -> {
+                        if ((!marker.equals("400") && !marker.equals("500"))
+                                && (a[0].equals("400") || a[0].equals("500"))) {
+                            marker = a[0];
+                            key = a[1];
+                            log.put(a[1], null);
+                        }
+                        if ((!marker.equals("200") && !marker.equals("300"))
+                                && (a[0].equals("200") || a[0].equals("300"))) {
+                            log.put(key, a[1]);
+                            marker = a[0];
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            for (Map.Entry<String, String> entry: log.entrySet()) {
+                out.println(entry.getKey() + ";" + entry.getValue());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("server.log", "unavailable.csv");
+    }
+}
