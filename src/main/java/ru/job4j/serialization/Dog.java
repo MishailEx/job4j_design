b4cd03a1c2421b88ca70ebe337142c42ff4948d8
MishailEx <Mishail.avram@mail.ru>
2021-10-02 10:02:56 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/Dog.java
+++ src/main/java/ru/job4j/serialization/Dog.java
@@ -2,6 +2,9 @@ package ru.job4j.serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -11,7 +14,9 @@ import javax.xml.bind.annotation.*;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @XmlRootElement(name = "dog")
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -38,6 +43,18 @@ public class Dog {
         this.crimes = crimes;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
     @Override
     public String toString() {
         return "Dog{" + "age=" + age
@@ -51,22 +68,38 @@ public class Dog {
         Dog dog = new Dog(2, "bobik", true,
                 new Parents("taksa", "bolonka"),
                 new String[] {"gnaw", "steals"});
-        JAXBContext context = JAXBContext.newInstance(Dog.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        String xml = "";
-        try (StringWriter stringWriter = new StringWriter()) {
-            marshaller.marshal(dog, stringWriter);
-            xml = stringWriter.getBuffer().toString();
-            System.out.println(xml);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        try (StringReader reader = new StringReader(xml)) {
-            Dog result = (Dog) unmarshaller.unmarshal(reader);
-            System.out.println(result);
-        }
+        JSONObject jsonParents = new JSONObject("{\"mather\" : \"bolonka\", \"father\" : \"taksa\"}");
+        List<String> list = new ArrayList<>();
+        list.add("steels");
+        list.add("gnaw");
+        JSONArray jsonCrimes = new JSONArray(list);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("age", dog.getAge());
+        jsonObject.put("sex", dog.isSex());
+        jsonObject.put("name", dog.getName());
+        jsonObject.put("parents", jsonParents);
+        jsonObject.put("crimes", jsonCrimes);
+        System.out.println(jsonObject);
+        System.out.println(new JSONObject(dog));
+
+
+//        JAXBContext context = JAXBContext.newInstance(Dog.class);
+//        Marshaller marshaller = context.createMarshaller();
+//        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+//        String xml = "";
+//        try (StringWriter stringWriter = new StringWriter()) {
+//            marshaller.marshal(dog, stringWriter);
+//            xml = stringWriter.getBuffer().toString();
+//            System.out.println(xml);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Unmarshaller unmarshaller = context.createUnmarshaller();
+//        try (StringReader reader = new StringReader(xml)) {
+//            Dog result = (Dog) unmarshaller.unmarshal(reader);
+//            System.out.println(result);
+//        }
+
 //        final Gson gson = new GsonBuilder().create();
 //        System.out.println(gson.toJson(dog));
 //
