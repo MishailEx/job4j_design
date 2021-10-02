629a6eaab76a291865382d30080ed5c3f1a4756f
MishailEx <Mishail.avram@mail.ru>
2021-10-02 09:40:46 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/Dog.java
+++ src/main/java/ru/job4j/serialization/Dog.java
@@ -3,14 +3,32 @@ package ru.job4j.serialization;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "dog")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Dog {
+    @XmlAttribute
     private int age;
+    @XmlAttribute
     private String name;
+    @XmlAttribute
     private boolean sex;
     private Parents parents;
+    @XmlElementWrapper
+    @XmlElement(name = "crimes")
     private String[] crimes;
+
+    public Dog() {
+    }
 
     public Dog(int age, String name, boolean sex, Parents parents, String[] crimes) {
         this.age = age;
@@ -29,17 +47,33 @@ public class Dog {
                 + ", crimes=" + Arrays.toString(crimes) + '}';
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         Dog dog = new Dog(2, "bobik", true,
                 new Parents("taksa", "bolonka"),
                 new String[] {"gnaw", "steals"});
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(dog));
-
-        String str = "{ \"age\":2,\"name\":\"bobik\",\"sex\":\"true\","
-                + "\"parents\":{\"father\":\"taksa\",\"mather\":\"bolonka\"},"
-                + "\"crimes\":[\"gnaw\",\"steels\"]}";
-        Dog dogFromJSON = gson.fromJson(str, Dog.class);
-        System.out.println(dogFromJSON);
+        JAXBContext context = JAXBContext.newInstance(Dog.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter stringWriter = new StringWriter()) {
+            marshaller.marshal(dog, stringWriter);
+            xml = stringWriter.getBuffer().toString();
+            System.out.println(xml);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Dog result = (Dog) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+//        final Gson gson = new GsonBuilder().create();
+//        System.out.println(gson.toJson(dog));
+//
+//        String str = "{ \"age\":2,\"name\":\"bobik\",\"sex\":\"true\","
+//                + "\"parents\":{\"father\":\"taksa\",\"mather\":\"bolonka\"},"
+//                + "\"crimes\":[\"gnaw\",\"steels\"]}";
+//        Dog dogFromJSON = gson.fromJson(str, Dog.class);
+//        System.out.println(dogFromJSON);
     }
 }
--- src/main/java/ru/job4j/serialization/Parents.java
+++ src/main/java/ru/job4j/serialization/Parents.java
@@ -1,8 +1,17 @@
 package ru.job4j.serialization;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "parents")
 public class Parents {
+    @XmlAttribute
     private String father;
+    @XmlAttribute
     private String mather;
+
+    public Parents() {
+    }
 
     public Parents(String father, String mather) {
         this.father = father;
