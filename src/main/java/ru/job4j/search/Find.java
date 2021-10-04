155f474b0b58f46cb4495ed046e5a1f9b3a7a52a
MishailEx <Mishail.avram@mail.ru>
2021-10-04 14:37:28 +0300

    2. Поиск файлов по критерию [#783 #211304]

--- src/main/java/ru/job4j/search/Find.java
+++ src/main/java/ru/job4j/search/Find.java
@@ -7,6 +7,8 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
@@ -14,14 +16,7 @@ import java.util.regex.Pattern;
 
 public class Find {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Not argument for search");
-        }
-        if (args.length < 4) {
-            throw new IllegalArgumentException("missing arguments. Must be -d=directory"
-                    + " -n=what search -t=type search -o=where write result");
-        }
-        ArgsName arg = ArgsName.of(args);
+        ArgsName arg = validateArgument(args);
         Path root = Paths.get(arg.get("d"));
         List<Path> toWrite = search(root, predicate(arg.get("t"), arg.get("n")));
         if (toWrite.isEmpty()) {
@@ -38,14 +33,38 @@ public class Find {
         }
     }
 
+    private static ArgsName validateArgument(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Not argument for search");
+        }
+        if (args.length != 4) {
+            throw new IllegalArgumentException("missing arguments. Must be -d=directory"
+                    + " -n=what search -t=type search -o=where write result");
+        }
+        ArgsName arg = ArgsName.of(args);
+        if (arg.get("d") == null || arg.get("n") == null
+                || arg.get("t") == null || arg.get("o") == null) {
+            throw new IllegalArgumentException("not argument");
+        }
+        if (!Files.isDirectory(Path.of(arg.get("d")))) {
+            throw new IllegalArgumentException(arg.get("d") + "not is directory");
+        }
+        if (arg.get("d").isEmpty() || arg.get("n").isEmpty()
+                || arg.get("t").isEmpty() || arg.get("o").isEmpty()) {
+            throw new IllegalArgumentException("not argument value");
+        }
+        return arg;
+    }
+
     private static Predicate<Path> predicate(String typeFind, String file) {
         Predicate<Path> predicate = null;
         if (validateT(typeFind)) {
             if (typeFind.equals("mask")) {
                 predicate = p -> {
-                    file.replace("*", ".*")
+                    String mask = file;
+                    mask.replace("*", ".*")
                             .replace("?", "\\w{1}");
-                    Pattern pattern = Pattern.compile(file);
+                    Pattern pattern = Pattern.compile(mask);
                     Matcher matcher = pattern.matcher(p.toFile().getName());
                     return matcher.find();
                 };
