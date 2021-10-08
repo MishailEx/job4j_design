501e52c18b9064d756b70069a522e3c789044026
MishailEx <83918375+MishailEx@users.noreply.github.com>
2021-10-08 19:40:22 +0300

    Add files via upload

---
+++ SQL/innPerson.sql
@@ -0,0 +1,10 @@
+create table inn(
+     id serial primary key,
+     number int
+ );
+
+ create table person(
+     id serial primary key,
+     name varchar(255),
+	 inn_id int references inn(id) unique
+ );
